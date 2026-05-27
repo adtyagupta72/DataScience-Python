7fca6f86b5ebfbf77edd9fe0890a19c234c88eab
Adtya Gupta <adtyagupta72@gmail.com>
2026-05-27 19:08:25 +0530

    Python: Data types WIP

---
+++ practice.py
@@ -0,0 +1,59 @@
+print("Hello Mindcoders!")
+
+# Adtya
+
+'''
+iwuebv aiweubv oiwquebv oiqwebuvoiquwebv iuqubwev 
+'''
+
+# print("Hello")
+# print("How")
+# print("Are")
+# print("You")
+# print("Adtya")
+
+# age = 4
+# print(age)
+# print(type(age))
+
+# age = "Four"
+# print(age)
+# print(type(age))
+
+# name = "Aditya"
+# profession = "Software developer"
+# experience = 10
+# print("Hello, I am", name, ". I am a ", profession, " professionally. And I have around ", experience, " years of experience with it!")
+
+x = 5
+print(x, type(x))
+x = "Hello World"
+print(x, type(x))
+x = 20
+print(x, type(x))
+x = 20.5
+print(x, type(x))
+x = 1j
+print(x, type(x))
+x = ["apple", "banana", "cherry", 3]
+print(x, type(x))
+x = ("apple", "banana", "cherry")
+print(x, type(x))
+x = range(6)
+print(x, type(x))
+x = {"name" : "John", "age" : 36}
+print(x, type(x))
+x = {"apple", "banana", "cherry", "apple"}
+print(x, type(x))
+x = frozenset({"apple", "banana", "cherry"})
+print(x, type(x))
+x = True		
+print(x, type(x))				
+x = b"Hello"					
+print(x, type(x))
+x = bytearray(5)				
+print(x, type(x))
+x = memoryview(bytes(5))		
+print(x, type(x))
+x = None
+print(x, type(x))
