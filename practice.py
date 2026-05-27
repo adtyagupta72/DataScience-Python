6f04498f8aa807fd8a6e1bd61f0acd7e8f275cab
Adtya Gupta <adtyagupta72@gmail.com>
2026-05-27 19:37:40 +0530

    Python: Operators WIP

--- practice.py
+++ practice.py
@@ -25,35 +25,44 @@ iwuebv aiweubv oiwquebv oiqwebuvoiquwebv iuqubwev
 # experience = 10
 # print("Hello, I am", name, ". I am a ", profession, " professionally. And I have around ", experience, " years of experience with it!")
 
-x = 5
-print(x, type(x))
-x = "Hello World"
-print(x, type(x))
-x = 20
-print(x, type(x))
-x = 20.5
-print(x, type(x))
-x = 1j
-print(x, type(x))
-x = ["apple", "banana", "cherry", 3]
-print(x, type(x))
-x = ("apple", "banana", "cherry")
-print(x, type(x))
-x = range(6)
-print(x, type(x))
-x = {"name" : "John", "age" : 36}
-print(x, type(x))
-x = {"apple", "banana", "cherry", "apple"}
-print(x, type(x))
-x = frozenset({"apple", "banana", "cherry"})
-print(x, type(x))
-x = True		
-print(x, type(x))				
-x = b"Hello"					
-print(x, type(x))
-x = bytearray(5)				
-print(x, type(x))
-x = memoryview(bytes(5))		
-print(x, type(x))
-x = None
-print(x, type(x))
+# x = 5
+# print(x, type(x))
+# x = "Hello World"
+# print(x, type(x))
+# x = 20
+# print(x, type(x))
+# x = 20.5
+# print(x, type(x))
+# x = 1j
+# print(x, type(x))
+# x = ["apple", "banana", "cherry", 3]
+# print(x, type(x))
+# x = ("apple", "banana", "cherry")
+# print(x, type(x))
+# x = range(6)
+# print(x, type(x))
+# x = {"name" : "John", "age" : 36}
+# print(x, type(x))
+# x = {"apple", "banana", "cherry", "apple"}
+# print(x, type(x))
+# x = frozenset({"apple", "banana", "cherry"})
+# print(x, type(x))
+# x = True		
+# print(x, type(x))				
+# x = b"Hello"					
+# print(x, type(x))
+# x = bytearray(5)				
+# print(x, type(x))
+# x = memoryview(bytes(5))		
+# print(x, type(x))
+# x = None
+# print(x, type(x))
+
+print("10 + 2 = ", 10 + 2)
+print("10 - 2 = ", 10 - 2)
+print("10 * 2 = ", 10 * 2)
+print("10 / 2 = ", 10 / 2)
+print("10 % 2 = ", 10 % 2)
+print("9 % 5 = ", 9 % 5)
+print("10 // 3 = ", 10 // 3)
+print("2 ** 3 = ", 2 ** 3)
