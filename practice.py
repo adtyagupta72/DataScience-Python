3e183f3991feafda027fde8553f863e7824344db
Adtya Gupta <adtyagupta72@gmail.com>
2026-06-10 19:16:26 +0530

    Python: Conditional execution WIP

--- practice.py
+++ practice.py
@@ -196,8 +196,36 @@ iwuebv aiweubv oiwquebv oiqwebuvoiquwebv iuqubwev
 
 # print(("+" + "-" * 10 + "+" + "\n")+ (("|" + ' ' * 10 + "|\n")*5) + ("+" + "-" * 10 + "+") )
 
-var = 11
+# var = 11
 
-if var == 11:
-    print("Var is 11")
-print("Hello")
+# if var == 11:
+#     print("Var is 11")
+# print("Hello")
+
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+
+# # if number1 > number2:
+# #     larger_number = number1
+# # else:
+# #     larger_number = number2
+
+# if number1 > number2: larger_number = number1
+# else: larger_number = number2
+
+# print("The larger number is:", larger_number)
+
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+ 
+largest_number = number1
+ 
+if number2 > largest_number:
+    largest_number = number2
+ 
+if number3 > largest_number:
+    largest_number = number3
+ 
+print("The largest number is:", largest_number)
