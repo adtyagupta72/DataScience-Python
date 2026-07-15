22420b5f470c75533e46ddd4d6b6f3a7006b749b
Adtya Gupta <adtyagupta72@gmail.com>
2026-07-15 19:45:45 +0530

    Python: List Comprehension

--- practice.py
+++ practice.py
@@ -1396,15 +1396,22 @@ index_inner(0-3) 0  1   2   3
 
 #List comprehension
 
-squares = [x ** 2 for x in range(10)]
+# squares = [x ** 2 for x in range(10)]
 
-print(squares)
+# print(squares)
 
-twos = [2 ** i for i in range(8)]
+# twos = [2 ** i for i in range(8)]
 
-print(twos)
+# print(twos)
 
-#[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
-odds = [x for x in squares if x % 2 != 0 ]
+# #[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# odds = [x for x in squares if x % 2 != 0 ]
 
-print(odds)
+# print(odds)
+
+board = []
+for i in range(8):
+    row = ["EMPTY" for i in range(8)]
+    board.append(row)
+
+print(board)
